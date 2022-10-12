23b272acc28aaa8ae24c4db9d66c26fa8aab6289
luciano <lpereira2306@gmail.com>
2022-10-12 16:02:32 -0300

    postUrls ready

--- src/constrollers/urlController.js
+++ src/constrollers/urlController.js
@@ -1,13 +1,48 @@
 import connection from '../postgres.js/postgres.js';
 import { nanoid } from 'nanoid';
+import Joi from 'joi';
+
+const urlSchema = Joi.object({
+	url: Joi.string()
+		.required()
+		.uri()
+		.regex(
+			/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+		),
+});
 
 const postUrl = async (req, res) => {
-	const token = req.headers.authorization?.replace('Bearer ', '');
-	let shortUrl = req.body;
-	shortUrl = nanoid();
+	if (!req.headers.authorization) {
+		return res.sendStatus(401);
+	}
 
-	console.log(token);
-	res.status(201).send(shortUrl);
+	const { url } = req.body;
+	const token = req.headers.authorization?.replace('Bearer ', '');
+	const validation = urlSchema.validate({ url }, { abortEarly: false });
+	const existe = await connection.query(
+		'SELECT * FROM session WHERE token = $1;',
+		[token]
+	);
+
+	try {
+		if (validation.error) {
+			const errors = validation.error.details
+				.map((value) => value.message)
+				.join(',');
+
+			return res.status(422).send(errors);
+		}
+		if (existe.rows.length === 0) {
+			return res.sendStatus(401);
+		}
+
+		let shortUrl = req.body;
+		shortUrl = nanoid();
+
+		res.status(201).send(shortUrl);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 export { postUrl };
